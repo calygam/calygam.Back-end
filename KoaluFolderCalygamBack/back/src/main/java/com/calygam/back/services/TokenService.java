907323f71fdedbbb27908a80e5623f0043fcd8a2
DevCaioBO <caiobuenooliveira1210@gmail.com>
2025-02-27 10:29:02 -0300

    feat: com o pom xml

--- KoaluFolderCalygamBack/back/src/main/java/com/calygam/back/services/TokenService.java
+++ KoaluFolderCalygamBack/back/src/main/java/com/calygam/back/services/TokenService.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
